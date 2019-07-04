fffb886047391697556d67981abcd301466a74b8
Mday1313 <melissaday076@gmail.com>
2019-07-04 15:26:21 -0400

    Designed, needs functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,34 @@ import React, { Component } from "react";
 import Wrapper from "./components/Wrapper";
 import Nav from "./components/Nav";
 import Title from "./components/Title";
+import cards from "./cards.json";
+import PlayingCards from "./components/PlayingCards";
+import "./index.css";
+
 
 class App extends Component {
 
-
+    state = {
+        cards
+      };
 
     render() {
         return(
             <div>
             <Nav></Nav>
+            <Title>Clicky Game</Title>
             <Wrapper>
-                <Title>Clicky Game</Title>
+               
+                
+                
+                {this.state.cards.map(card => (
+                    <PlayingCards
+                    id={card.id}
+                    key={card.id}
+                    image={card.image} 
+                    />
+                ))}
+               
             </Wrapper>
             </div>
         );
