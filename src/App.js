57a0fc6c5a5c3e488b50bc82ee027335cc527813
Mday1313 <melissaday076@gmail.com>
2019-07-05 10:13:51 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Wrapper from "./components/Wrapper";
 import Nav from "./components/Nav";
 import Title from "./components/Title";
-import cards from "./cards.json";
+import cards from "./cards.json.js.js";
 import PlayingCards from "./components/PlayingCards";
 import "./index.css";
 
